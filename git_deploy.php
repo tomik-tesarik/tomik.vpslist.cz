bd65df6db40b782122375e7b805e220ced5b63a8
Tomik Tesarik <tomik.tesarik@gmail.com>
2012-12-29 23:16:57 +0100

    log dest

--- git_deploy.php
+++ git_deploy.php
@@ -6,7 +6,7 @@ try {
 }
 
 //log the request
-file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+file_put_contents('../tmp/logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 
 
 if ($payload->ref === 'refs/heads/master') {
